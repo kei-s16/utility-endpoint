c67141188935f65642b948219b1c4842933b03ae
kei-s16 <kei1216s@nifty.com>
2023-09-30 17:16:45 +0900

    ビルドできるように型をただしく

--- utils/time/main.ts
+++ utils/time/main.ts
@@ -1,6 +1,6 @@
 import { datetime } from './deps.ts'
 
-export function now(tz: String = "Asia/Tokyo") : String {
+export function now(tz: string = "Asia/Tokyo") : string {
   const now = datetime().toZonedTime(tz);
   return now.toISO();
 }
